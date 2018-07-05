f9dbf0895861df69330c2f3607bc2ff8da935c84
GrigRUSS <grigruss@ya.ru>
2018-07-05 13:58:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,12 @@ read -r REMOTEHOST
 if [[ -z "$REMOTEHOST" ]]; then
         echo -e "\\e[33;1mWRONG! Run script again and type remote host correct!\\[0m"
 fi
-echo "Type exists path for clearbckp.sh on the remote computer [/home/$REMOTEUSER/bin/]"
+echo "Type remote path for backups on the remote computer [/home/$REMOTEUSER/backups/]:"
+read -r REMOTEPATH
+if [[ -z "$REMOTEPATH" ]]; then
+        REMOTEPATH="/home/$REMOTEUSER/backups/"
+fi
+echo "Type exists path for clearbckp.sh on the remote computer [/home/$REMOTEUSER/bin/]:"
 read -r RPATH
 if [[ -z "$RPATH" ]]; then
         RPATH="/home/$REMOTEUSER/bin"
@@ -23,6 +28,15 @@ fi
 # Copy public ssh-key to remote computer (need remote password)
 scp "$HOME/.ssh/id_rsa.pub" "$REMOTEUSER@$REMOTEHOST:/home/$REMOTEUSER/.ssh/authorized_keys"
 
+sed "
+s/REMOTEUSER=\"user\"/REMOTEUSER=\"$REMOTEUSER\"/
+s/REMOTEHOST=\"example.com\"/REMOTEHOST=\"$REMOTEHOST\"/
+s/REMOTEPATH=\"/home/backupsdir/\"/REMOTEPATH=\"$REMOTEUSER\"/
+" ./backup.sh > ./tmp
+mv ./tmp ./backup.sh
+sed "s/BDIR=\"/home/\$USER/backups\"/BDIR=\"$REMOTEPATH\"/" ./clearbckp.sh > ./tmp
+mv ./tmp ./clearbckp.sh
+
 if [[ ! -d "$HOME/bin" ]]; then
         mkdir -p "$HOME/bin"
 fi
@@ -34,6 +48,12 @@ cp ./backup.sh "$HOME/bin/backup.sh"
 chmod +x "$HOME/bin/backup.sh"
 
 # Example target for cron. At 01:00 AM every night.
-echo "0 1 * * * $HOME/bin/backup.sh">>/var/spool/cron/root
+echo "0 1 * * * $HOME/bin/backup.sh" >> /var/spool/cron/root
 
 scp -B ./clearbckp.sh "$REMOTEUSER@$REMOTEHOST:$RPATH"
+
+echo "On the remote computer run:
+        export EDITOR=nano | crontab -e
+add type next string
+        0 1 * * * $RPATH/clearbckp.sh
+save and exit."
